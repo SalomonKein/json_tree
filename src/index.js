417653d9d73f00d5a5b320f8583b02f1c7536e90
SalomonKein <61635747+SalomonKein@users.noreply.github.com>
2021-09-21 00:23:20 +0300

    add https

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import {ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://countries.trevorblades.com//graphql",
+  uri: "https://countries.trevorblades.com//graphql",
   cache: new InMemoryCache({
     addTypename: false,
   }),
